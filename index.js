7f55a6e623c06d5d71a800ce04f524b61dd30467
Ved Nakum <126437129+byteved@users.noreply.github.com>
2023-06-07 15:02:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 
+// Get the draggable options and the drop zone
+const options = document.querySelectorAll('.option');
+const dropzone = document.querySelector('.dropzone');
+
+// Add event listeners to the options for drag events
+options.forEach(option => {
+  option.addEventListener('dragstart', dragStart);
+  option.addEventListener('dragend', dragEnd);
+});
+
+// Add event listeners to the drop zone for drop events
+dropzone.addEventListener('dragover', dragOver);
+dropzone.addEventListener('dragenter', dragEnter);
+dropzone.addEventListener('dragleave', dragLeave);
+dropzone.addEventListener('drop', dragDrop);
+
+// Drag functions
+function dragStart(event) {
+  event.dataTransfer.setData('text', event.target.id);
+}
+
+function dragEnd() {
+  // Do any necessary cleanup or visual changes after dragging ends
+}
+
+// Drop zone functions
+function dragOver(event) {
+  event.preventDefault();
+}
+
+function dragEnter(event) {
+  event.preventDefault();
+  dropzone.style.backgroundColor = '#999';
+}
+
+function dragLeave() {
+  dropzone.style.backgroundColor = '#ccc';
+}
+
+function dragDrop(event) {
+  event.preventDefault();
+  dropzone.style.backgroundColor = '#ccc';
+
+  const optionId = event.dataTransfer.getData('text');
+  const option = document.getElementById(optionId);
+
+  // Check if the selected option is correct and update the drop zone text
+  if (option.innerText === 'Option 2') {
+    dropzone.innerText = 'Correct!';
+    dropzone.style.backgroundColor = 'green';
+  } else {
+    dropzone.innerText = 'Incorrect!';
+    dropzone.style.backgroundColor = 'red';
+  }
+}
