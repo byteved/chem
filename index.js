904411986372584742d1fa6aac3e0a11aacb673e
VedNakum <113805164+VedNakum@users.noreply.github.com>
2023-06-07 18:17:55 -0400

    e

--- index.js
+++ index.js
@@ -1,91 +1,117 @@
-const slideContainer = document.getElementById('slide-container');
-const slides = document.getElementsByClassName('slide');
-const selectButton = document.getElementById('select-button');
-const nextButton = document.getElementById('next-button');
-
 // Array of levels/questions
-const levels = [
+// Array of question sets for each level
+const questionSets = [
   {
-    question: 'Question 1',
-    options: [
-      { text: 'Option 1', isCorrect: true },
-      { text: 'Option 2', isCorrect: false },
-      { text: 'Option 3', isCorrect: false },
-      { text: 'Option 4', isCorrect: false },
-    ],
+    level: 1,
+    questions: [
+      {
+        question: "Question 1 - Level 1",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: 0
+      },
+      {
+        question: "Question 2 - Level 1",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: 1
+      },
+      // Add more questions for level 1
+    ]
   },
-  // Add more levels here...
+  {
+    level: 2,
+    questions: [
+      {
+        question: "Question 1 - Level 2",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: 2
+      },
+      {
+        question: "Question 2 - Level 2",
+        options: ["Option 1", "Option 2", "Option 3", "Option 4"],
+        answer: 3
+      },
+      // Add more questions for level 2
+    ]
+  },
+  // Add more levels
 ];
 
+// Track the current level and question index
 let currentLevel = 0;
+let currentQuestionIndex = 0;
 
-// Initialize level
-function initializeLevel(levelIndex) {
-  const level = levels[levelIndex];
-  const question = document.getElementById('question');
-  const options = document.getElementById('options');
-  const feedback = document.getElementById('feedback');
+// Function to load the current question
+function loadQuestion() {
+  const currentQuestion = questionSets[currentLevel].questions[currentQuestionIndex];
+  const questionElement = document.querySelector(".question");
+  const optionsContainer = document.querySelector(".options");
 
-  question.innerHTML = level.question;
-  options.innerHTML = '';
-  feedback.innerHTML = '';
-  selectButton.classList.remove('hidden');
-  nextButton.classList.add('hidden');
+  // Update the question text
+  questionElement.textContent = currentQuestion.question;
 
-  // Create option buttons
-  level.options.forEach((option, index) => {
-    const optionButton = document.createElement('button');
-    optionButton.classList.add('option');
-    optionButton.innerText = option.text;
-    optionButton.dataset.index = index;
-    optionButton.addEventListener('click', () => {
-      handleOptionClick(index);
-    });
-    options.appendChild(optionButton);
+  // Clear previous options
+  optionsContainer.innerHTML = "";
+
+  // Create the new options
+  currentQuestion.options.forEach((option, index) => {
+    const button = document.createElement("button");
+    button.classList.add("option");
+    button.textContent = option;
+    optionsContainer.appendChild(button);
+
+    // Add click event listener to each option
+    button.addEventListener("click", () => handleOptionClick(index));
   });
 }
 
-// Handle option button click
-function handleOptionClick(index) {
-  const selectedOption = levels[currentLevel].options[index];
+// Function to handle option click
+function handleOptionClick(selectedOption) {
+  const currentQuestion = questionSets[currentLevel].questions[currentQuestionIndex];
+  const options = document.querySelectorAll(".option");
 
-  // Add green outline for correct option
-  const options = document.getElementsByClassName('option');
-  Array.from(options).forEach((option, i) => {
-    if (i === index && selectedOption.isCorrect) {
-      option.classList.add('correct');
-    } else {
-      option.classList.remove('correct');
-    }
-  });
+  // Disable clicking on options
+  options.forEach(option => option.removeEventListener("click", handleOptionClick));
 
-  // Show feedback
-  const feedback = document.getElementById('feedback');
-  feedback.innerHTML = selectedOption.isCorrect ? 'Correct!' : 'Incorrect!';
-
-  // Hide select button, show next button
-  selectButton.classList.add('hidden');
-  nextButton.classList.remove('hidden');
-}
-
-// Handle next button click
-nextButton.addEventListener('click', () => {
-  slides[currentLevel].classList.add('hidden');
-  currentLevel++;
-  if (currentLevel < levels.length) {
-    slides[currentLevel].classList.remove('hidden');
-  } else {
-    // All levels completed
-    const question = document.getElementById('question');
-    const options = document.getElementById('options');
-    const feedback = document.getElementById('feedback');
-    question.innerHTML = 'All levels completed!';
-    options.innerHTML = '';
-    feedback.innerHTML = '';
-    selectButton.classList.add('hidden');
-    nextButton.classList.add('hidden');
+  // Show feedback for selected option
+  if (selectedOption === currentQuestion.answer) {
+    options[selectedOption].classList.add("correct");
   }
-});
 
-// Initialize first level
-initializeLevel(currentLevel);
+  // Show the next button
+  const selectButton = document.querySelector(".select-button");
+  selectButton.classList.add("hidden");
+  const nextButton = document.querySelector(".next-button");
+  nextButton.classList.remove("hidden");
+}
+
+// Function to load the next question or level
+function loadNext() {
+  const nextButton = document.querySelector(".next-button");
+
+  // If there are more questions in the current level
+  if (currentQuestionIndex < questionSets[currentLevel].questions.length - 1) {
+    currentQuestionIndex++;
+    loadQuestion();
+    nextButton.classList.add("hidden");
+  } else {
+    // If there are more levels
+    if (currentLevel < questionSets.length - 1) {
+      currentLevel++;
+      currentQuestionIndex = 0;
+      loadQuestion();
+      nextButton.classList.add("hidden");
+    } else {
+      // If all levels and questions are completed
+      // You can add the code here for what to do when the quiz is finished
+    }
+  }
+}
+
+// Event listener for next button click
+const nextButton = document.querySelector(".next-button");
+nextButton.addEventListener("click", loadNext);
+
+// Start the quiz by loading the initial question
+loadQuestion();
+
+
